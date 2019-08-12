e47d708904803a2377e82819cf5f47e4573cabd3
henrypuentesg <hpuente79161@universidadean.edu.co>
2019-08-12 08:47:55 -0500

    Se Ajusta Ejerciicio 8

--- EjerciciosGuia1/src/ejercicios/Ejercicio08.java
+++ EjerciciosGuia1/src/ejercicios/Ejercicio08.java
@@ -1,5 +1,60 @@
 package ejercicios;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Ejercicio08 {
+	
+	
+	public Integer cantidadPersonas ;
+	public static final double VALOR_PASAJE = 10000;
+	public static final int CANTIDAD_PERSONAS_COLECTIVO = 50;
+	
+	public Integer getCantidadPersonas() {
+		return cantidadPersonas;
+	}
+	public void setCantidadPersonas(Integer cantidadPersonas) {
+		this.cantidadPersonas = cantidadPersonas;
+	}
+	public static double getValorPasaje() {
+		return VALOR_PASAJE;
+	}
+	public static double getCantidadPersonasColectivo() {
+		return CANTIDAD_PERSONAS_COLECTIVO;
+	}
+	
+	public Ejercicio08(Integer cantidadPersonas) {
+		super();
+		this.cantidadPersonas = cantidadPersonas;
+	}
+
+	
+	public void calculoViaje() {
+		
+		List<Integer> cantidadPasajeros = new ArrayList<Integer>();
+		int cantiad=100;
+		for(int i = 1;i<=100;i+=50) {
+			cantidad = cantidad-50;
+			
+			
+			
+		}
+		
+		
+		
+		int cantidadViajes = getCantidadPersonas()/CANTIDAD_PERSONAS_COLECTIVO;
+		int cantidadPersonasViajes = 
+		System.out.println("Cantidad viajes: " +cantidadViajes );
+		
+		for(int i = 1;cantidadViajes )
+		
+		
+		
+		
+	}
+	
+	
+	
+	
 
 }
--- EjerciciosGuia1/src/main/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-/**
- * @author henry
- *
- */
-package main;
